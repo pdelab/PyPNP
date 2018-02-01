4f5535eac1b67e2c9376052e27d4c4c85ae3bf2e
Arthur Bousquet <arth.bousquet@gmail.com>
2018-02-01 01:14:10 -0500

    quick update

--- params.py
+++ params.py
@@ -7,6 +7,8 @@
 CLEAN = 'yes'
 DATA_DIR = "output/data_pnp/"
 IMG_DIR = "output/img_pnp/"
+PrintFig = 1                 # it prints every PrintFig newton's iterations
+PrintData = 1                # it prints every PrintData newton's iterations
 
 # Mesh
 Nx = 25
@@ -27,3 +29,17 @@ qn = -1.0
 coordinates = [0, 0, 0]
 lower_values = [0, -2.0, -1.0]
 upper_values = [-2.0, 0.0, 1.0]
+
+# Linear Solver
+linear_precon = 'ilu'
+linear_tol = 1E-8
+linear_itmax = 100
+gmres_restart = 10
+nonzero_initial_guess = False
+monitor_convergence = False
+
+# Newton Solver
+tol = 1E-8
+itmax = 20
+show = 2                        # How much to print 0, 1, 2
+Residual = "relative"           # or "true"
